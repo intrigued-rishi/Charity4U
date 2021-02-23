632e852e0bf2cd5cd04ba45b36d231e012743e9f
jaydip1235 <jaydipdeyalok@gmail.com>
2021-02-23 18:56:03 +0530

    Update NodeMailer.ts

--- src/Utils/NodeMailer.ts
+++ src/Utils/NodeMailer.ts
@@ -6,14 +6,14 @@ export class NodeMailer {
         return nodeMailer.createTransport(SendGrid({
             service: 'SendGrid',
             auth: {
-                api_key: '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'
+                api_key: ':)'
             }
         }));
     }
 
     static sendEmail(data: { to: string[] , subject: string, html: string}): Promise<any> {
         return NodeMailer.initializeTransport().sendMail({
-            from: 'jaydipdey2807@gmail.com',
+            from: 'abc@gmail.com',
             to: data.to,
             subject: data.subject,
             html: data.html

e6cb0189d2c95ec0c6c6a3021882655c2341e851
jaydip1235 <jaydipdeyalok@gmail.com>
2021-02-23 18:54:48 +0530

    Update org.env.ts

--- src/environment/org.env.ts
+++ src/environment/org.env.ts
@@ -1,7 +1,7 @@
 import {Environment} from "./env";
 
 export const OrgEnvironment : Environment = {
-    db_url : 'mongodb+srv://mongodbuser:myuser@mongodb.2gpch.mongodb.net?retryWrites=true&w=majority',
-   // jwt_secret : 'token'
-    jwt_secret : 'organization'
+    db_url : 'mongodb url',
+  
+    jwt_secret : ':)'
 };
